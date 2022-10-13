ee4a04b92e1a3037aed9038c8003acda16a0dd0e
vijayakumar58 <vijayalingam58@gmail.com>
2022-10-13 13:28:55 +0530

    add html js

---
+++ script.js
@@ -0,0 +1,8 @@
+function foo(){
+    var fn=document.getElementById("firstname").value;
+    var mn=document.getElementById("middlename").value;
+    var ln=document.getElementById("lastname").value;
+    var em=document.getElementById("email").value;
+    var ps=document.getElementById("password").value;
+    console.log(fn,mn,ln,em,ps);
+}
